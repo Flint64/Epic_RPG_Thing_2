c23c525b605df9372a6269775e650f1d2af3b36b
Flint64 <twda96@gmail.com>
2019-07-10 09:38:31 -0600

    Added more to the combat layout and added story layout/class

--- app/src/main/java/chimera/epic_rpg_thing/view/combatActivity.java
+++ app/src/main/java/chimera/epic_rpg_thing/view/combatActivity.java
@@ -3,6 +3,12 @@ package chimera.epic_rpg_thing.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import chimera.epic_rpg_thing.R;
 
@@ -12,5 +18,69 @@ public class combatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combat);
+
+        final Button attackButton = findViewById(R.id.attack);
+        final Button itemButton = findViewById(R.id.item);
+        final Button magicButton = findViewById(R.id.magic);
+        final Button voteButton = findViewById(R.id.voteButton);
+        final Button fleeButton = findViewById(R.id.fleeButton);
+        final Button potionButton = findViewById(R.id.potionButton);
+        final Button reviveButton = findViewById(R.id.reviveButton);
+        final Button backButton = findViewById(R.id.backButton);
+        final TextView playerInfo = findViewById(R.id.playerInfo);
+        final LinearLayout itemLayout = findViewById(R.id.itemLayout);
+
+        //TODO: Add bool checks to each of the item/magic/attack buttons to see if the new view is visible so that the back button can "close" them out by hiding stuff right
+        //TODO: Also figure out how to set a layout dynamically to prevent me having to stack all of these menus on top of each other
+
+        attackButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "I don't do anything yet!", Toast.LENGTH_SHORT).show();
+        }});
+
+        itemButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                potionButton.setVisibility(View.VISIBLE);
+                reviveButton.setVisibility(View.VISIBLE);
+                itemLayout.setVisibility(View.VISIBLE);
+                backButton.setVisibility(View.VISIBLE);
+                playerInfo.setVisibility(View.INVISIBLE);
+                attackButton.setVisibility(View.INVISIBLE);
+                magicButton.setVisibility(View.INVISIBLE);
+                itemButton.setVisibility(View.INVISIBLE);
+
+                itemLayout.setX(100);
+                itemLayout.setY(700);
+            }});
+
+        magicButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "I don't do anything yet!", Toast.LENGTH_SHORT).show();
+            }});
+
+        voteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "I don't do anything yet!", Toast.LENGTH_SHORT).show();
+            }});
+
+        fleeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "I don't do anything yet!", Toast.LENGTH_SHORT).show();
+            }});
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+//                if (potionButton.getVisibility() == View.INVISIBLE){
+                    potionButton.setVisibility(View.INVISIBLE);
+                    reviveButton.setVisibility(View.INVISIBLE);
+                    itemLayout.setVisibility(View.INVISIBLE);
+                    backButton.setVisibility(View.INVISIBLE);
+                    playerInfo.setVisibility(View.VISIBLE);
+                    attackButton.setVisibility(View.VISIBLE);
+                    magicButton.setVisibility(View.VISIBLE);
+                    itemButton.setVisibility(View.VISIBLE);
+//                }
+            }});
+
     }
 }
--- app/src/main/java/chimera/epic_rpg_thing/view/storyActivity.java
+++ app/src/main/java/chimera/epic_rpg_thing/view/storyActivity.java
@@ -1,7 +1,13 @@
 package chimera.epic_rpg_thing.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import chimera.epic_rpg_thing.R;
 
@@ -13,9 +19,50 @@ public class storyActivity extends AppCompatActivity {
     //TODO: This is going to have to have fragments of inventory, skills, combat, story & a combat listener to check if in combat to make
     //      changes to inventory and skills to allow/disallow doing things if you are/aren't in combat
 
+    private short pressed = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        final Button continueButton = findViewById(R.id.continueButton);
+        final TextView storyText = findViewById(R.id.storyText);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            public void onClick(View view) {
+                pressed++;
+
+                switch(pressed){
+                    case 1:
+                        storyText.setText("The king's armies and their elite, the holy Knights of the Sacred Eye, forever the Watchers and Warders of evil, fought valiantly to defend the kingdom and castle, but they too, fell victim to Ichor's claws, and some became unholy servants of the night as they fell in defense of their home. There was one, however, who managed to survive this onslaught: The Princess' own personal bodyguard, apprentice Watcher Glenn. He, along with the princess herself, managed to flee the castle and retreat to safety.");
+                    break;
+
+                    case 2:
+                        storyText.setText("It is here, months after the destruction of the kingdom that we pick up our story, here, where our four valiant heroes, wanderers to our once fair land, have pledged to help Princess Joyeuse and her noble companion Sir Glenn to rid the kingdom of the scourge of the Ichor, and return peace to the land.");
+                    break;
+
+                    case 3:
+                        storyText.setText("You embark on your journey together, well aware of the dangers you will face on the way, ready to fight your way to peace - and riches untold.");
+                    break;
+
+                    case 4:
+                        storyText.setText("As you begin your travels, you encounter a group of enemies! Prepare yourself!");
+                        continueButton.setText("Fight!");
+                        break;
+
+                    case 5:
+                        startCombat();
+                    break;
+                }
+
+            }});
+    }
+
+    //Starts the combat activity
+    private void startCombat() {
+        Intent i = new Intent(this, combatActivity.class);
+        startActivity(i);
     }
 }
