1629e11156406f80eaec0c3ad37e2a15263953e3
Flint64 <twda96@gmail.com>
2019-07-11 17:47:08 -0600

    refined the layout (on my phone at least) (more of the same)

--- app/src/main/java/chimera/epic_rpg_thing/view/combatActivity.java
+++ app/src/main/java/chimera/epic_rpg_thing/view/combatActivity.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import chimera.epic_rpg_thing.R;
+import chimera.epic_rpg_thing.presenter.CombatPresenter;
 
 public class combatActivity extends AppCompatActivity {
+
+    boolean attackBtn = false;
+    boolean itemBtn = false;
+    boolean magicBtn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combat);
+
+        CombatPresenter cp = new CombatPresenter();
 
         final Button attackButton = findViewById(R.id.attack);
         final Button itemButton = findViewById(R.id.item);
@@ -29,9 +38,11 @@ public class combatActivity extends AppCompatActivity {
         final Button backButton = findViewById(R.id.backButton);
         final TextView playerInfo = findViewById(R.id.playerInfo);
         final LinearLayout itemLayout = findViewById(R.id.itemLayout);
+        final LinearLayout btnLayout = findViewById(R.id.btnLayout);
 
         //TODO: Add bool checks to each of the item/magic/attack buttons to see if the new view is visible so that the back button can "close" them out by hiding stuff right
         //TODO: Also figure out how to set a layout dynamically to prevent me having to stack all of these menus on top of each other
+
 
         attackButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -40,15 +51,19 @@ public class combatActivity extends AppCompatActivity {
 
         itemButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                itemBtn = true;
+
                 potionButton.setVisibility(View.VISIBLE);
                 reviveButton.setVisibility(View.VISIBLE);
                 itemLayout.setVisibility(View.VISIBLE);
+
+                btnLayout.addView(potionButton);
+
                 backButton.setVisibility(View.VISIBLE);
                 playerInfo.setVisibility(View.INVISIBLE);
                 attackButton.setVisibility(View.INVISIBLE);
                 magicButton.setVisibility(View.INVISIBLE);
                 itemButton.setVisibility(View.INVISIBLE);
-
                 itemLayout.setX(120);
                 itemLayout.setY(600);
             }});
@@ -68,18 +83,29 @@ public class combatActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "I don't do anything yet!", Toast.LENGTH_SHORT).show();
             }});
 
+
         backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-//                if (potionButton.getVisibility() == View.INVISIBLE){
+                if (itemBtn){
                     potionButton.setVisibility(View.INVISIBLE);
                     reviveButton.setVisibility(View.INVISIBLE);
-                    itemLayout.setVisibility(View.INVISIBLE);
-                    backButton.setVisibility(View.INVISIBLE);
-                    playerInfo.setVisibility(View.VISIBLE);
-                    attackButton.setVisibility(View.VISIBLE);
-                    magicButton.setVisibility(View.VISIBLE);
-                    itemButton.setVisibility(View.VISIBLE);
-//                }
+                }
+
+                if (attackBtn){
+
+                }
+
+                if (magicBtn){
+
+                }
+
+                itemLayout.setVisibility(View.INVISIBLE);
+                backButton.setVisibility(View.INVISIBLE);
+                playerInfo.setVisibility(View.VISIBLE);
+                attackButton.setVisibility(View.VISIBLE);
+                magicButton.setVisibility(View.VISIBLE);
+                itemButton.setVisibility(View.VISIBLE);
+
             }});
 
     }
